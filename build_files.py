4e0c84b577d3935dfd86f7d0cc079b3401ff4d9d
Sanjay-R <sanjayrangrej6@gmail.com>
2025-11-30 00:47:44 +0530

    Vercel ready – clean structure

--- build_files.py
+++ build_files.py
@@ -1,4 +1,3 @@
-# This file runs during Vercel build to collect static files
 import os
 from django.conf import settings
 from django.core.management import call_command
@@ -6,5 +5,4 @@ from django.core.management import call_command
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 import django
 django.setup()
-
-call_command('collectstatic', '--noinput', verbosity=0)
+call_command('collectstatic', '--noinput')
