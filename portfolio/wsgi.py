795025814b8f91b9ff058b72654733e9e42cc48a
Sanjay-R <sanjayrangrej6@gmail.com>
2025-11-30 00:57:54 +0530

    Fix Vercel: export application variable

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -1,8 +1,12 @@
 """
-WSGI config for portfolio project.
+wsgi.py – Fixed for Vercel (2025 working version)
 """
+
 import os
 from django.core.wsgi import get_wsgi_application
 
+# This line is REQUIRED for Vercel
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+
+# This variable name MUST be "application" (Vercel looks for it)
 application = get_wsgi_application()
